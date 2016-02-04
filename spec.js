28de710b3581edf1f6a2c591c421594dca11a9c6
vincent.sue <gnux123@gmail.com>
2016-02-04 17:16:11 +0800

    註解及多餘程式碼移除.

--- spec.js
+++ spec.js
@@ -1,13 +1,14 @@
+//設定mocha chai的使用
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 
 var expect = chai.expect;
 
+//情境設定
 describe('網站驗證項目-首頁：', function(){
 
 	browser.ignoreSynchronization = true;
-
 
 	beforeEach(function(){
 		browser.get('/');
@@ -15,11 +16,14 @@ describe('網站驗證項目-首頁：', function(){
 		browser.sleep(3000);
 	});
 
+	//確認網站標題
 	it("確認是否有標題", function() {
 		var title = browser.getTitle();
 		expect(title).to.eventually.eq('美國新蛋直購');
 	});
 
+
+	//確認選單項目數
 	it("確認主選單項目數",function(){
 		var menuTitleText = $('.toolbar h3').getText();
 		// console.log(menuTitleText);
@@ -29,35 +33,4 @@ describe('網站驗證項目-首頁：', function(){
 		expect(menuList).to.eventually.eq(13);
 	});
 
-	// it('should have a  history',function(){
-	// 	add(1,2);
-	// 	add(3,4);
-	//
-	// 	expect(history.count()).toEqual(2);
-	//
-	// 	add(5, 6);
-	//
-	// 	expect(history.count()).toEqual(3);
-	//
-	// });
-
-	// it('should add one and two', function(){
-	//
-	// 	firstNum.sendKeys(1);
-	// 	secondNum.sendKeys(2);
-	//
-	// 	goBtn.click();
-	//
-	// 	expect(results.getText()).toEqual('3');
-	// });
-	//
-	// it('should add four and six', function(){
-	//
-	// 	firstNum.sendKeys(4);
-	// 	secondNum.sendKeys(6);
-	//
-	// 	goBtn.click();
-	//
-	// 	expect(results.getText()).toEqual('10');
-	// });
 });
